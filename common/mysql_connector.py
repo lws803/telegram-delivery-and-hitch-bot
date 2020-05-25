ba996e42812eca15128f50f9042d2fbd5588b0bf
lws803 <lws803@gmail.com>
2020-05-25 11:57:55 +0800

    singleton approach for mysql connector

--- common/mysql_connector.py
+++ common/mysql_connector.py
@@ -17,17 +17,23 @@ logger = logging.getLogger(__name__)
 
 class MySQLConnector:
     def __init__(self):
+        self._start_session()
+
+    def _start_session(self):
         mysql_engine = create_engine(
             os.environ.get('MYSQL_PROD')
         )
         try:
-            self.Session = sessionmaker(bind=mysql_engine)
+            self.session = sessionmaker(bind=mysql_engine)
         except Exception:
             logger.warning(str(Exception))
 
+
     @contextmanager
     def session(self):
-        session = self.Session()
+        if not self.session:
+            self._start_session()
+        session = self.session()
         try:
             yield session
             session.commit()
