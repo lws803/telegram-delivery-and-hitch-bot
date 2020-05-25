8385792aa97e5347a3975a1b8f8f383ac192ac5d
lws803 <lws803@gmail.com>
2020-05-25 13:03:12 +0800

    fixing mysql

--- common/mysql_connector.py
+++ common/mysql_connector.py
@@ -14,24 +14,17 @@ logger = logging.getLogger(__name__)
 
 class MySQLConnector:
     def __init__(self):
-        self.session = None
-        self._start_session()
-
-    def _start_session(self):
         mysql_engine = create_engine(
             os.environ.get('MYSQL_PROD')
         )
         try:
-            self.session = sessionmaker(bind=mysql_engine)
+            self.Session = sessionmaker(bind=mysql_engine)
         except Exception:
             logger.warning(str(Exception))
 
-
     @contextmanager
     def session(self):
-        if not self.session:
-            self._start_session()
-        session = self.session()
+        session = self.Session()
         try:
             yield session
             session.commit()
